93ad15289f843575805ac83cbd8ed578529af58d
liviaalmeida <leave.ah@gmail.com>
2020-05-05 01:31:01 -0300

    Making URI robust

--- blipy/Types/__URI.py
+++ blipy/Types/__URI.py
@@ -1,13 +1,28 @@
-from urllib.parse import quote
+from urllib.parse import quote, urlparse, unquote
+
+BLIP_TAKE_MAX = 100
 
 class URI:
 	@staticmethod
-	def new(baseuri, params = None):
-		checkForParams = baseuri.split('?')
-		base = checkForParams[0]
-		if params:
-			keyEqualsValue = lambda key: f'{key}={quote(params[key])}'
-			base += '?' + '&'.join(map(keyEqualsValue, params))
-		if len(checkForParams) == 2:
-			base += ('' if params else '?') + '&' + checkForParams[1]
-		return base
+	def new(baseuri, params = {}):
+		parsed = urlparse(baseuri)
+		path, query = unquote(parsed.path), parsed.query
+		if len(query) > 0:
+			originalParams = URI.parametrize(query)
+			params.update(originalParams)
+		uri = quote(path)
+		if len(params.keys()) > 0:
+			uri += f'?{URI.queryStringfy(params)}'
+		return uri
+
+	@staticmethod
+	def queryStringfy(params):
+		return '&'.join(map(lambda key: f'{key}={quote(params[key])}', params))
+	
+	@staticmethod
+	def parametrize(queryString):
+		params = {}
+		for query in queryString.split('&'):
+			key, value = query.split('=')
+			params[key] = unquote(value)
+		return params
